fc706e6bf85910afb00ed63f14d9a0cc17badff8
Eser Ozvataf <eser@ozvataf.com>
2015-09-16 10:32:23 +0300

    * initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Scabbia2 Config Component
+
+[This component](https://github.com/scabbiafw/scabbia2-config/) is a configuration compiler which supports binding multiple configuration files.
+
+[![Build Status](https://travis-ci.org/scabbiafw/scabbia2-config.png?branch=master)](https://travis-ci.org/scabbiafw/scabbia2-config)
+[![Total Downloads](https://poser.pugx.org/scabbiafw/scabbia2-config/downloads.png)](https://packagist.org/packages/scabbiafw/scabbia2-config)
+[![Latest Stable Version](https://poser.pugx.org/scabbiafw/scabbia2-config/v/stable)](https://packagist.org/packages/scabbiafw/scabbia2-config)
+[![Latest Unstable Version](https://poser.pugx.org/scabbiafw/scabbia2-config/v/unstable)](https://packagist.org/packages/scabbiafw/scabbia2-config)
+[![License](https://poser.pugx.org/scabbiafw/scabbia2-config/license.png)](https://packagist.org/packages/scabbiafw/scabbia2-config)
+
+## Links
+- [Documentation](http://scabbiafw.com/docs/)
+- [Twitter](https://twitter.com/scabbiafw)
+- [Contributor List](contributors.md)
+- License Information [I](LICENSE-Apache) [II](LICENSE-Flask)
+
+
+## Contributing
+It is publicly open for any contribution. Bugfixes, new features and extra modules are welcome. All contributions should be filed on the [scabbiafw/scabbia2-config](https://github.com/scabbiafw/scabbia2-config) repository.
+
+* To contribute to code: Fork the repo, push your changes to your fork, and submit a pull request.
+* To report a bug: If something does not work, please report it using GitHub issues.
+* To support: [![Donate](https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BXNMWG56V6LYS)
---
+++ src/Config/ConfigCollection.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Scabbia2 Config Component
+ * http://www.scabbiafw.com/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link        https://github.com/scabbiafw/scabbia2-config for the canonical source repository
+ * @copyright   2010-2015 Scabbia Framework Organization. (http://www.scabbiafw.com/)
+ * @license     http://www.apache.org/licenses/LICENSE-2.0 - Apache License, Version 2.0
+ */
+
+namespace Scabbia\Config;
+
+/**
+ * ConfigCollection
+ *
+ * @package     Scabbia\Config
+ * @author      Eser Ozvataf <eser@ozvataf.com>
+ * @since       2.0.0
+ */
+class ConfigCollection
+{
+    /** @type int NONE      no flag */
+    const NONE = 0;
+    /** @type int OVERWRITE overwrite existing nodes by default */
+    const OVERWRITE = 1;
+    /** @type int FLATTEN   flatten nodes by default */
+    const FLATTEN = 2;
+
+
+    /** @type array configuration flags */
+    public $configFlags = [
+        "disabled" => false // always false
+    ];
+    /** @type array configuration content */
+    public $content = [];
+
+
+    /**
+     * Adds a piece into configuration compilation
+     *
+     * @param string $uConfig configuration
+     * @param int    $uFlags  loading flags
+     *
+     * @return void
+     */
+    public function add($uConfig, $uFlags = self::NONE)
+    {
+        $this->process($this->content, $uConfig, $uFlags);
+    }
+
+    /**
+     * Returns route information in order to store it
+     *
+     * @return array
+     */
+    public function save()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Processes the configuration file in order to simplify its accessibility
+     *
+     * @param mixed $uTarget  target reference
+     * @param mixed $uNode    source object
+     * @param int   $uFlags   loading flags
+     *
+     * @return void
+     */
+    protected function process(&$uTarget, $uNode, $uFlags)
+    {
+        $tQueue = [
+            [[], $uNode, $uFlags, &$uTarget, null, false]
+        ];
+
+        do {
+            $tItem = array_pop($tQueue);
+
+            if ($tItem[4] === null) {
+                $tRef = &$tItem[3];
+            } else {
+                $tRef = &$tItem[3][$tItem[4]];
+            }
+
+            if (is_scalar($tItem[1]) || $tItem[1] === null) {
+                if (!isset($tRef) || ($tItem[2] & self::OVERWRITE) === self::OVERWRITE) {
+                    $tRef = $tItem[1];
+                }
+
+                continue;
+            }
+
+            if (!is_array($tRef) || ($tItem[2] & self::OVERWRITE) === self::OVERWRITE) {
+                $tRef = []; // initialize as an empty array
+            }
+
+            foreach ($tItem[1] as $tKey => $tSubnode) {
+                $tFlags = $tItem[2];
+                $tListNode = false;
+
+                $tNodeParts = explode("|", $tKey);
+                $tNodeKey = array_shift($tNodeParts);
+
+                if ($tItem[5] && is_numeric($tNodeKey)) {
+                    $tNodeKey = count($tRef);
+                }
+
+                foreach ($tNodeParts as $tNodePart) {
+                    if (array_key_exists($tNodePart, $this->configFlags)) {
+                        if ($this->configFlags[$tNodePart] !== true) {
+                            continue 2;
+                        }
+                    } elseif ($tNodePart === "list") {
+                        $tListNode = true;
+                    } elseif ($tNodePart === "important") {
+                        $tFlags |= self::OVERWRITE;
+                    } elseif ($tNodePart === "flat") {
+                        $tFlags |= self::FLATTEN;
+                    }
+                }
+
+                $tNewNodeKey = $tItem[0];
+                if (($tFlags & self::FLATTEN) === self::FLATTEN) {
+                    $tNodeKey = ltrim("{$tItem[4]}/{$tNodeKey}", "/");
+                    $tNewNodeKey[] = $tNodeKey;
+
+                    $tQueue[] = [$tNewNodeKey, $tSubnode, $tFlags, &$tRef, $tNodeKey, $tListNode];
+                } else {
+                    $tNewNodeKey[] = $tNodeKey;
+                    $tQueue[] = [$tNewNodeKey, $tSubnode, $tFlags, &$tRef[$tNodeKey], null, $tListNode];
+                }
+            }
+        } while (count($tQueue) > 0);
+    }
+}
